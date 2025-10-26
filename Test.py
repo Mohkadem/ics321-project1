8b3f47395fc7a5818fd07ef0f7a5f9252220fd59
Mohkadem <s202245820@kfupm.edu.sa>
2025-10-26 16:42:56 +0300

    Simple modificationclear

--- Test.py
+++ Test.py
@@ -10,7 +10,7 @@ def create_connection():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Mohammed32",
+            password="",
             database="racing"
         )
         return connection
